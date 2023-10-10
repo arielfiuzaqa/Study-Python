db32799c28d55f76037d649031e15bf2eef22c88
Fontenele <arielfiuzaqa@gmail.com>
2023-10-10 13:28:51 -0300

    Adicionado novos desafios dentro do arquivo loop_for.py

--- aprendizados/loop_for.py
+++ aprendizados/loop_for.py
@@ -51,6 +51,43 @@ for explosao in range(1, 11):
     print(f'Explosão em {explosao}')
 
 
-'''
+''' Loops aninhados - geralmente os loops usamos um unico interavel mas em alguns
+casos podemos acabar tendo que usar mais de um interavel.
+    Como se fosse uma lista ou uma faixa de valores por exemplo.
 
+    Exemplo prático: Estamos criando um sistema de calendário e precisamos digitar a seguinte 
+    combinação para todos os países do mundo:
+    Nome do País + Estação. 
+
+    Podemos criar uma lista com todos os países mas vamos usar 3 para exemplificar.
 '''
+print('\nLoops Aninhados\n')
+paises = ['Brasil', 'India', 'EUA']
+estacao_do_ano = ['primavera', 'verão', 'outono', 'inverno']
+# Para cada pais dentro da lista paises iremos percorrer as estações do ano usando um loop for
+for pais in paises:
+    for estacao in estacao_do_ano:
+        print(f'{pais} está na estação {estacao}.')
+'''É perceptível a lógica bela desse código. 
+Vai percorrer cada item pais => paises individualmente e onde cada pais => estacao e como
+estacao esta para => estacao_do_ano, percorre tudo antes de voltar e pegar o segundo item 
+pais => paises.
+'''
+# Na forma numerica do exemplo anterior
+print('\nExemplo Numérico\n')
+for x in range(1, 11):
+    for y in range(1, 4):
+        print(f'Valor externo {x} e interno {y}')
+
+''' 🥇🥇🥇 Desafio 3 🥇🥇🥇 - Sabemos que os celulares que são lançados hoje em dia tem sempre
+as versões mais simples ate as versões premium, pois bem. Vamos agora usar isso para indicar os
+celulares que serao lançados no futuro.
+'''
+print('\nDesafio 3\n')
+celulares = ['Asus', 'Sansung', 'Sony', 'IPhone']
+versoes = ['Plus', 'Premium Plus', 'Premium Deluxe', 'Plus Premium Ultra']
+# Percorrendo todas as posições das listas celular e versão
+for celular in celulares:
+    for versao in versoes:
+        print(f'{celular} {versao}')
+
