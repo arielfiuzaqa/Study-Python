c065deebab4d3ec6516e8570701871cdc9149b5d
Fontenele <arielfiuzaqa@gmail.com>
2023-10-24 00:34:12 -0300

    Adicionado alguns exercicios no relacionados aos arquivos .json

--- use-json/app.py
+++ use-json/app.py
@@ -36,6 +36,39 @@ with open('use-json/desafio.json', encoding='utf-8') as arquivo_json:
     print(data["user"][1]["orders"][0]["total"])
 
 
+''' Como criar ou ler arquivos JSON no python '''
+import json
+# Criar ou ler JSON existente
+print('\nCriar ou Ler JOSON existente\n')
+computador_json = """ {
+    "marca": "Dell",
+    "preço": "15000"
+} """
+# String JSON
+data = json.loads(computador_json)
+print(data["preço"])
+# Salvar um string JSON -> Arquivo JSON
+with open('computador_json','w',encoding='utf-8') as arquivo_json:
+    json.dump(computador_json, arquivo_json)
+# Para Ler um arquivo JSON
+with open('computador_json', encoding='utf-8') as arquivo_json:
+    string_computador_json = json.load(arquivo_json) # Convertendo JSON -> String
+    dicionario_computado_json = json.loads(string_computador_json) # Converte string em dicionario
+    print(dicionario_computado_json["marca"])
 
+# Desafio #2 - Criando JSON
+print('\nDesafio #2 - Criando JSON\n')
+import json
+# Criar ou ler arquivo JSON existente
+usuarios_json = """{
+    "nome":"Jonny Dell",
+    "age":"3000",
+    "city":"Nova AU",
+    "isStudent": true,
+    "gpa": 3.5
+}"""
+# Salvar um string JSON -> Arquivo JSON
+with open('desafio_json','w',encoding='utf-8') as arquivo_json:
+    json.dump(usuarios_json, arquivo_json)
 
 
